2b24ee521a2a54ad8b78ba2df65e39b77751489b
Destinyside <destinyside@163.com>
2017-04-27 22:37:40 +0800

    add commitments

--- restas/restashello.lisp
+++ restas/restashello.lisp
@@ -1,15 +1,20 @@
+;;; add require for "ASDF"
 (require 'ecl-quicklisp)
+
+;;; load restas
 (asdf:operate 'asdf:load-op '#:restas)
 
+;;; use restas define a module
 (restas:define-module #:restas.hello-world
 		      (:use :cl))
-
+;;;
 (in-package #:restas.hello-world)
 
+;;; define a route 
 (restas:define-route main ("")
-		     "<h1>Hello world!</h1>\
+		     "<h1>Hello world!</h1> \
 		     <a href=\"/hello\">hello</a>")
 (restas:define-route hello ("/hello")
 		     "<h1>You succeed in forwarding to path /hello</h1>")
-
+;;; start the server
 (restas:start '#:restas.hello-world :port 8080)
