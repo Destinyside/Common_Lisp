4c4d01509585fbb26a68e4dfb3e99645d82e1fc2
Destinyside <destinyside@163.com>
2017-12-04 09:26:43 +0000

    format and cond

---
+++ test/conditiontest.lisp
@@ -0,0 +1,3 @@
+
+;;; signaling handling restarting
+;;; 
---
+++ test/formattest.lisp
@@ -0,0 +1,18 @@
+
+;;; ~a ~A ~% ~c ~d ~f
+;;; ~x ~o ~b ~R
+;;; ~F ~E ~G ~$
+;;; ~r
+;;; ~p
+;;; list:~{~} ~^ (remove the last comma etc)  ~[cond1;cond2~]
+
+;;; ~ @ : # , 
+;;; ~; ~* ~? ~/
+
+(format t "~30$~%" pi) ;;~v$ ~#$
+
+(format t "~,5f~%" pi)
+
+(format t "~d~%" 100000)
+(format t "~:d~%" 100000000)
+
