e1b05eacc4a88f3bee7d4b0eefb277aed4bab13e
Destinyside <destinyside@163.com>
2017-12-09 07:15:55 +0000

    test html

--- restas/htmltest.lisp
+++ restas/htmltest.lisp
@@ -1,2 +1,13 @@
-(defun generate-html (tags output attr)
-  (format t "<~A ~A>~A</~A>" tags attr output tags))
+;;; add require for "ASDF"
+#+ecl (require 'ecl-quicklisp)
+#+(or clisp) (load "~/quicklisp/setup.lisp")
+
+(asdf:oos 'asdf:load-op '#:cl-who)
+
+(defmacro with-html (&body body)
+  `(cl-who:with-html-output-to-string (*standard-output* nil :prologue t)
+                               ,@body))
+
+
+
+(format t "~A~%" (with-html (:html (:body (:p :class "text-danger" "test")))))
